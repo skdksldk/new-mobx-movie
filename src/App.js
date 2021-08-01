962c12e135f8d32451657c021b4d23cf9996ab3e
skdksldk <tnwns1485@naver.com>
2021-08-01 15:55:41 +0900

    add material-ui header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import MoviePage from './pages/MoviePage';
+import Header from './component/Header';
 import './App.css';
 
 
 class App extends Component {
-    render() {
-        return ( <
-            div className = "App" >
-            <
-            MoviePage / > < /div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MoviePage />
+      </div>
+    );
+  }
 }
 
 export default App;
